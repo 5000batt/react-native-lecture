9ec3758b30abe20a1c0913e9af3b11638f8ac887
5000batt <5000batt@gmail.com>
2021-02-10 15:09:02 +0900

    react-native component

---
+++ components/SelectedItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// 선택한 아이템 한 개를 속성으로 내려 받음
+const SelectedItem = ({ item }) => {
+  return (
+    <View>
+      <Text>{ item.id } { item.title }</Text>
+    </View>
+  )
+}
+
+export default SelectedItem;
